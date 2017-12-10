9a42fa5e7a9db3ba2a37d7fbe86507b9c39a3751
Varuna Singh <varuna.singh@gmail.com>
2017-12-10 18:48:25 +0400

    Removed core for Xml testing.  XmlPullParsing for GWT working okay for basic parsing. In progress..

--- app-gwt/src/main/java/com/ustadmobile/port/gwt/client/application/ApplicationPresenter.java
+++ app-gwt/src/main/java/com/ustadmobile/port/gwt/client/application/ApplicationPresenter.java
@@ -2,13 +2,21 @@ package com.ustadmobile.port.gwt.client.application;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.IOException;
 import java.util.Hashtable;
 
 //testing deferred binding
 import com.ustadmobile.port.gwt.client.util.ReplaceWithThis;
-
+import com.ustadmobile.port.gwt.xmlpull.XmlPullParserGWT;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.xml.client.DOMException;
+import com.google.gwt.xml.client.Document;
+import com.google.gwt.xml.client.Element;
+import com.google.gwt.xml.client.Node;
+import com.google.gwt.xml.client.XMLParser;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.Presenter;
@@ -87,36 +95,63 @@ public class ApplicationPresenter
         ReplaceWithThis bindingTest = new ReplaceWithThis();
         GWT.log(bindingTest.value);
         
-        GWT.log("Testing some core stuff..");
+        //GWT.log("Testing some core stuff..");
         
         //UstadMobileSystemImpl impl = UstadMobileSystemImpl.getInstance();
-        TestThis testThis = new TestThis();
-        GWT.log("This value is from core:" + testThis.value);
-        GWT.log("Implemented.");
+        //TestThis testThis = new TestThis();
+        //GWT.log("This value is from core:" + testThis.value);
+        //GWT.log("Implemented.");
+        
+        //GWT.log("Testing GWT's XML");
+        String testXmlString = "<?xml version=\"1.0\" ?>"
+        		+ "<message>"
+        		+ 	"<header>"
+        		+ 		"<to displayName=\"Varuna\" address=\"varuna@ustadmobile.com\" />"
+        		+ 		"<from displayName=\"Mike\" address=\"mike@ustadmobile.com\" />"
+        		+ 		"<sent>2017-12-07T12:03:55Z</sent>"
+        		+ 		"<subject>Re: Hello</subject>"
+        		+ 	"</header>"
+        		+ 	"<body>Hello!</body>"
+        		+ "</message>";
+        
+        String attr = parseMessage(testXmlString, "from", "displayName");
+        //GWT.log("XML Result: " + attr);
+        
+        
+        GWT.log("");
+        GWT.log("GWT XmlPullParsing .. ");
+        XmlPullParserGWT xmlObj = new XmlPullParserGWT(testXmlString);
+        
+        try {
+        	int evt;
+			while((evt = xmlObj.next()) != XmlPullParser.END_DOCUMENT){
+				GWT.log("Parsing: going next..");
+				String tagName = "";
+				if(evt == XmlPullParser.START_TAG){
+					tagName = xmlObj.getName();
+					GWT.log("START_TAG : " + tagName);
+				}else if(evt == XmlPullParser.TEXT) {
+					GWT.log("TEXT: \"" + xmlObj.getText() + "\"");
+				}else if(evt == XmlPullParser.END_TAG) {
+					tagName = xmlObj.getName();
+					GWT.log("END_TAG: " + tagName);
+				}else {
+					GWT.log("Other event: " + evt);
+				}
+				
+			}
+		} catch (XmlPullParserException | IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			GWT.log("Unable to loop thorugh XML.");
+		}
+        
         
         //If you have, 
         //view.setUiHanders(this);
     }
-    //*/
-    
-    
-    /*
-    //Just trying:
-    @Inject
-    ApplicationPresenter(
-            EventBus eventBus,
-            MyView view,
-            MyProxy proxy,
-            PlaceManager placeManager,
-            ContactsWidgetPresenter contactsWidgetPresenter) {
-        super(eventBus, view, proxy, RevealType.Root);
-        
-        this.placeManager = placeManager;
-        
-        //If you have, 
-        //view.setUiHanders(this);
-    }
-    */
+
+  
     
     //Seperate out reveal
     
@@ -139,5 +174,30 @@ public class ApplicationPresenter
 		 placeManager.revealPlace(placeRequest);
     			 
     }
+    
+    /**
+     * Testing XML Parsing
+     * @param messageXml
+     * @param tag
+     * @param attribute
+     * @return
+     */
+    private String parseMessage(String messageXml, String tag, String attribute) {
+    	  try {
+    	    // parse the XML document into a DOM
+    	    Document messageDom = XMLParser.parse(messageXml);
+
+    	    // find the sender's display name in an attribute of the <from> tag
+    	    Node tagNode = messageDom.getElementsByTagName(tag).item(0);
+    	    String attributeValue = ((Element)tagNode).getAttribute(attribute);
+    	    
+    	    return attributeValue;
+    	    
+    	  } catch (DOMException e) {
+    	    GWT.log("Could not parse XML document.");
+    	  }
+    	  
+    	  return null;
+    	}
     
 }
