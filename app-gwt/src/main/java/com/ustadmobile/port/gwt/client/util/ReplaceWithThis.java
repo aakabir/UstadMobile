64a97407e9003f379bab575ec0d42be35dac054e
Varuna Singh <varuna.singh@gmail.com>
2018-01-07 22:30:44 +0300

    Pushing progress so far (wont compile yet)

--- app-gwt/src/main/java/com/ustadmobile/port/gwt/client/util/ReplaceWithThis.java
+++ app-gwt/src/main/java/com/ustadmobile/port/gwt/client/util/ReplaceWithThis.java
@@ -1,12 +1,10 @@
 package com.ustadmobile.port.gwt.client.util;
-import java.util.Calendar;
 
 public class ReplaceWithThis {
 
 	public String value;
 	
 	public ReplaceWithThis(){
-		Calendar cal;
 		value = "OLD VALUE";
 	}
 }
