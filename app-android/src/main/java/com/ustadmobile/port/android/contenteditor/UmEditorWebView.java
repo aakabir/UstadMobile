a223864086c025629038cd217aa46ed219dc7c25
Lukundo Kileha <lkileha@gmail.com>
2018-11-29 16:54:37 +0400

    Added custom weview which dispatch key events

--- app-android/src/main/java/com/ustadmobile/port/android/contenteditor/UmEditorWebView.java
+++ app-android/src/main/java/com/ustadmobile/port/android/contenteditor/UmEditorWebView.java
@@ -1,4 +1,46 @@
 package com.ustadmobile.port.android.contenteditor;
 
-public class UmEditorWebView {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.inputmethod.BaseInputConnection;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.webkit.WebView;
+
+/**
+ * Custom webView which handles dispatch all key events to the JS side.
+ */
+public class UmEditorWebView  extends WebView {
+
+    public UmEditorWebView(Context context) {
+        super(context);
+    }
+
+    public UmEditorWebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public UmEditorWebView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * In order for the webView to dispatch all key events, this is a must.
+     * @param outAttrs
+     * @return
+     */
+    @Override
+    public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
+        return new BaseInputConnection(this, false);
+    }
+
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        return super.dispatchKeyEvent(event);
+    }
+
+
+
 }
