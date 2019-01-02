68fade9ce3f58988766963891ba2f52e7709eac7
Lukundo Kileha <lkileha@gmail.com>
2019-01-02 21:36:09 +0400

    Added method signatures to help out on popup menus and toolbar menus formatting

--- app-android/src/main/java/com/ustadmobile/port/android/contenteditor/ContentFormattingHelper.java
+++ app-android/src/main/java/com/ustadmobile/port/android/contenteditor/ContentFormattingHelper.java
@@ -4,6 +4,7 @@ import com.toughra.ustadmobile.R;
 import com.ustadmobile.core.view.ContentEditorView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -20,8 +21,6 @@ import static com.ustadmobile.core.view.ContentEditorView.*;
  *     Use {@link ContentFormattingHelper#getFormatByCommand(String)} to get
  *     a content formatting by its command name.
  *
- *     Use {@link ContentFormattingHelper#getAllFormats()} to get the list of all content formats
- *
  *     Use {@link ContentFormattingHelper#updateFormat(ContentFormat)} to update specific
  *     content format. i.e when active status changes.
  * </p>
@@ -32,11 +31,30 @@ import static com.ustadmobile.core.view.ContentEditorView.*;
  */
 public class ContentFormattingHelper {
 
+    /**
+     * Flag to indicate all text formats
+     */
     private static final int FORMATTING_TEXT_INDEX = 0;
 
+    /**
+     * Flag to indicate all paragraph formats
+     */
     private static final int FORMATTING_PARAGRAPH_INDEX = 1;
 
-    public static final int FORMATTING_ACTIONS_INDEX = 2;
+    /**
+     * Flag to indicate all font formats
+     */
+    private static final int FORMATTING_FONT_INDEX = 2;
+
+    /**
+     * Flag to indicate all toolbar action formats
+     */
+    public static final int ACTIONS_TOOLBAR_INDEX = 3;
+
+    /**
+     * Flag to indicate all language directionality formats
+     */
+    private static final int LANGUAGE_DIRECTIONALITY = 4;
 
     private static ContentFormattingHelper instance;
 
@@ -46,6 +64,10 @@ public class ContentFormattingHelper {
 
     private List<StateChangeDispatcher> dispatcherList = new CopyOnWriteArrayList<>();
 
+    /**
+     * Get ContentFormattingHelper instance
+     * @return New/Existing ContentFormattingHelper instance
+     */
     public static ContentFormattingHelper getInstance() {
         if(instance == null){
             instance = new ContentFormattingHelper();
@@ -54,6 +76,7 @@ public class ContentFormattingHelper {
         }
         return instance;
     }
+
 
     /**
      * Construct quick action menu items
@@ -66,37 +89,14 @@ public class ContentFormattingHelper {
     private static List<ContentFormat> prepareQuickActions(){
         List<ContentFormat> quickActions = new ArrayList<>();
         try{
-            ContentFormat bold = getFormatByCommand(TEXT_FORMAT_TYPE_BOLD);
-            bold.setFormatId(R.id.content_action_bold);
-            quickActions.add(bold);
-
-            ContentFormat italic = getFormatByCommand(TEXT_FORMAT_TYPE_ITALIC);
-            italic.setFormatId(R.id.content_action_italic);
-            quickActions.add(italic);
-
-            ContentFormat underline = getFormatByCommand(TEXT_FORMAT_TYPE_UNDERLINE);
-            underline.setFormatId(R.id.content_action_underline);
-            quickActions.add(underline);
-
-            ContentFormat strikeThrough = getFormatByCommand(TEXT_FORMAT_TYPE_STRIKE);
-            strikeThrough.setFormatId(R.id.content_action_strike_through);
-            quickActions.add(strikeThrough);
-
-            ContentFormat ordered = getFormatByCommand(PARAGRAPH_FORMAT_LIST_ORDERED);
-            ordered.setFormatId(R.id.content_action_ordered_list);
-            quickActions.add(ordered);
-
-            ContentFormat unordered = getFormatByCommand(PARAGRAPH_FORMAT_LIST_UNORDERED);
-            unordered.setFormatId(R.id.content_action_uordered_list);
-            quickActions.add(unordered);
-
-            ContentFormat iIncrease = getFormatByCommand(PARAGRAPH_FORMAT_INDENT_INCREASE);
-            iIncrease.setFormatId(R.id.content_action_indent);
-            quickActions.add(iIncrease);
-
-            ContentFormat iDecrease = getFormatByCommand(PARAGRAPH_FORMAT_INDENT_DECREASE);
-            iDecrease.setFormatId(R.id.content_action_outdent);
-            quickActions.add(iDecrease);
+            quickActions.add(getFormatByCommand(TEXT_FORMAT_TYPE_BOLD));
+            quickActions.add(getFormatByCommand(TEXT_FORMAT_TYPE_ITALIC));
+            quickActions.add(getFormatByCommand(TEXT_FORMAT_TYPE_UNDERLINE));
+            quickActions.add(getFormatByCommand(TEXT_FORMAT_TYPE_STRIKE));
+            quickActions.add(getFormatByCommand(PARAGRAPH_FORMAT_LIST_ORDERED));
+            quickActions.add(getFormatByCommand(PARAGRAPH_FORMAT_LIST_UNORDERED));
+            quickActions.add(getFormatByCommand(PARAGRAPH_FORMAT_INDENT_INCREASE));
+            quickActions.add(getFormatByCommand(PARAGRAPH_FORMAT_INDENT_DECREASE));
         }catch (NullPointerException e){
             e.printStackTrace();
         }
@@ -107,20 +107,27 @@ public class ContentFormattingHelper {
         List<ContentFormat> mText = new ArrayList<>();
         List<ContentFormat> mDirection = new ArrayList<>();
         List<ContentFormat> mParagraph = new ArrayList<>();
-        mText.add(new ContentFormat(R.drawable.ic_format_bold_black_24dp,
-                TEXT_FORMAT_TYPE_BOLD,false,FORMATTING_TEXT_INDEX));
-        mText.add(new ContentFormat(R.drawable.ic_format_italic_black_24dp,
-                TEXT_FORMAT_TYPE_ITALIC,false,FORMATTING_TEXT_INDEX));
+        List<ContentFormat> mToolbar = new ArrayList<>();
+
+        List<ContentFormat> mFont = new ArrayList<>();
+
+        mText.add(new ContentFormat(R.drawable.ic_format_bold_black_24dp, TEXT_FORMAT_TYPE_BOLD,
+                false,FORMATTING_TEXT_INDEX, R.id.content_action_bold));
+        mText.add(new ContentFormat(R.drawable.ic_format_italic_black_24dp, TEXT_FORMAT_TYPE_ITALIC,
+                false,FORMATTING_TEXT_INDEX,R.id.content_action_italic));
         mText.add(new ContentFormat(R.drawable.ic_format_underlined_black_24dp,
-                TEXT_FORMAT_TYPE_UNDERLINE,false,FORMATTING_TEXT_INDEX));
+                TEXT_FORMAT_TYPE_UNDERLINE,false,FORMATTING_TEXT_INDEX,
+                R.id.content_action_underline));
         mText.add(new ContentFormat(R.drawable.ic_format_strikethrough_black_24dp,
-                TEXT_FORMAT_TYPE_STRIKE,false,FORMATTING_TEXT_INDEX));
+                TEXT_FORMAT_TYPE_STRIKE,false,FORMATTING_TEXT_INDEX,
+                R.id.content_action_strike_through));
         mText.add(new ContentFormat(R.drawable.ic_format_size_black_24dp,
                 TEXT_FORMAT_TYPE_FONT,false,FORMATTING_TEXT_INDEX));
         mText.add(new ContentFormat(R.drawable.ic_number_superscript,
                 TEXT_FORMAT_TYPE_SUP,false,FORMATTING_TEXT_INDEX));
         mText.add(new ContentFormat(R.drawable.ic_number_subscript,
                 TEXT_FORMAT_TYPE_SUB,false,FORMATTING_TEXT_INDEX));
+
         mParagraph.add(new ContentFormat(R.drawable.ic_format_align_justify_black_24dp,
                 PARAGRAPH_FORMAT_ALIGN_JUSTIFY, false,FORMATTING_PARAGRAPH_INDEX));
         mParagraph.add(new ContentFormat(R.drawable.ic_format_align_right_black_24dp,
@@ -130,21 +137,63 @@ public class ContentFormattingHelper {
         mParagraph.add(new ContentFormat(R.drawable.ic_format_align_left_black_24dp,
                 PARAGRAPH_FORMAT_ALIGN_LEFT,false,FORMATTING_PARAGRAPH_INDEX));
         mParagraph.add(new ContentFormat(R.drawable.ic_format_list_numbered_black_24dp,
-                PARAGRAPH_FORMAT_LIST_ORDERED,false,FORMATTING_PARAGRAPH_INDEX));
+                PARAGRAPH_FORMAT_LIST_ORDERED,false,FORMATTING_PARAGRAPH_INDEX,
+                R.id.content_action_ordered_list));
         mParagraph.add(new ContentFormat(R.drawable.ic_format_list_bulleted_black_24dp,
-                PARAGRAPH_FORMAT_LIST_UNORDERED,false,FORMATTING_PARAGRAPH_INDEX));
+                PARAGRAPH_FORMAT_LIST_UNORDERED,false,FORMATTING_PARAGRAPH_INDEX,
+                R.id.content_action_uordered_list));
         mParagraph.add(new ContentFormat(R.drawable.ic_format_indent_increase_black_24dp,
-                PARAGRAPH_FORMAT_INDENT_INCREASE,false,FORMATTING_PARAGRAPH_INDEX));
+                PARAGRAPH_FORMAT_INDENT_INCREASE,false,FORMATTING_PARAGRAPH_INDEX,
+                R.id.content_action_indent));
         mParagraph.add(new ContentFormat(R.drawable.ic_format_indent_decrease_black_24dp,
-                PARAGRAPH_FORMAT_INDENT_DECREASE,false,FORMATTING_PARAGRAPH_INDEX));
+                PARAGRAPH_FORMAT_INDENT_DECREASE,false,FORMATTING_PARAGRAPH_INDEX,
+                R.id.content_action_outdent));
+
         mDirection.add(new ContentFormat(R.drawable.ic_format_textdirection_l_to_r_white_24dp,
-                ACTION_TEXT_DIRECTION_LTR,false,FORMATTING_ACTIONS_INDEX));
+                ACTION_TEXT_DIRECTION_LTR,true,LANGUAGE_DIRECTIONALITY,
+                R.id.direction_rightToLeft, R.string.content_direction_rtl));
         mDirection.add(new ContentFormat(R.drawable.ic_format_textdirection_r_to_l_white_24dp,
-                ACTION_TEXT_DIRECTION_RTL,false,FORMATTING_ACTIONS_INDEX));
+                ACTION_TEXT_DIRECTION_RTL,false,LANGUAGE_DIRECTIONALITY,
+                R.id.direction_leftToRight,R.string.content_direction_ltr));
+
+        mFont.add(new ContentFormat(0, TEXT_FORMAT_TYPE_FONT,false,
+                FORMATTING_FONT_INDEX, 8,R.string.content_font_8));
+        mFont.add(new ContentFormat(0, TEXT_FORMAT_TYPE_FONT,false,
+                FORMATTING_FONT_INDEX, 10,R.string.content_font_10));
+        mFont.add(new ContentFormat(0, TEXT_FORMAT_TYPE_FONT,false,
+                FORMATTING_FONT_INDEX, 12,R.string.content_font_12));
+        mFont.add(new ContentFormat(0, TEXT_FORMAT_TYPE_FONT,false,
+                FORMATTING_FONT_INDEX, 14,R.string.content_font_14));
+        mFont.add(new ContentFormat(0, TEXT_FORMAT_TYPE_FONT,false,
+                FORMATTING_FONT_INDEX, 18, R.string.content_font_18));
+        mFont.add(new ContentFormat(0, TEXT_FORMAT_TYPE_FONT,false,
+                FORMATTING_FONT_INDEX, 24,R.string.content_font_24));
+        mFont.add(new ContentFormat(0, TEXT_FORMAT_TYPE_FONT,false,
+                FORMATTING_FONT_INDEX, 36,R.string.content_font_36));
+
+
+        mToolbar.add(new ContentFormat(R.drawable.ic_undo_white_24dp,
+                ACTION_UNDO,false,ACTIONS_TOOLBAR_INDEX,R.id.content_action_undo));
+        mToolbar.add(new ContentFormat(R.drawable.ic_redo_white_24dp,
+                ACTION_REDO,false,ACTIONS_TOOLBAR_INDEX,R.id.content_action_redo));
+        mToolbar.add(new ContentFormat(R.drawable.ic_text_format_white_24dp,
+                "",false,ACTIONS_TOOLBAR_INDEX,R.id.content_action_format));
+        mToolbar.add(new ContentFormat(R.drawable.fab_add,
+                "",false,ACTIONS_TOOLBAR_INDEX,R.id.content_action_insert));
+        mToolbar.add(new ContentFormat(R.drawable.ic_document_preview,
+                "",false,ACTIONS_TOOLBAR_INDEX,R.id.content_action_preview));
+        mToolbar.add(new ContentFormat(R.drawable.ic_format_textdirection_l_to_r_white_24dp,
+                ACTION_TEXT_DIRECTION_LTR,false,ACTIONS_TOOLBAR_INDEX,
+                R.id.content_action_direction));
+        mToolbar.add(new ContentFormat(R.drawable.ic_menu_white_24dp,
+                "",false,ACTIONS_TOOLBAR_INDEX,R.id.content_action_pages));
+
         List<ContentFormat> allFormats = new ArrayList<>();
         allFormats.addAll(mText);
         allFormats.addAll(mParagraph);
         allFormats.addAll(mDirection);
+        allFormats.addAll(mToolbar);
+        allFormats.addAll(mFont);
         return allFormats;
     }
 
@@ -161,6 +210,22 @@ public class ContentFormattingHelper {
             }
         }
         return formats;
+    }
+
+    /**
+     * Get list of all formats by type
+     * @param formatId id to be found
+     * @return found list of all formats of
+     */
+    public ContentFormat getFormatById(int formatId,int formatType) {
+        ContentFormat contentFormat = null;
+        for(ContentFormat format: getFormatListByType(formatType)){
+            if(format.getFormatId() == formatId){
+                contentFormat = format;
+                break;
+            }
+        }
+        return contentFormat;
     }
 
     /**
@@ -193,6 +258,40 @@ public class ContentFormattingHelper {
             }
         }
         dispatchUpdate(contentFormat);
+    }
+
+
+    /**
+     * Get list of all directionality formatting
+     * @param activeFormat Directionality format to be activated
+     * @return List of all formats
+     */
+    public  List<ContentFormat> getLanguageDirectionalityList(ContentFormat activeFormat){
+        List<ContentFormat> directionality = getFormatListByType(LANGUAGE_DIRECTIONALITY);
+        for(ContentFormat format: directionality){
+            if(activeFormat != null){
+                directionality.get(directionality.indexOf(format))
+                        .setActive(format.getFormatId() == activeFormat.getFormatId());
+
+            }
+        }
+        return directionality;
+    }
+
+    /**
+     * Get all font format list
+     * @param activeFormat Font format to be activated
+     * @return List of all fonts
+     */
+    public  List<ContentFormat> getFontList(ContentFormat activeFormat){
+        List<ContentFormat> fontList = getFormatListByType(FORMATTING_FONT_INDEX);
+        for(ContentFormat format: fontList){
+            if(activeFormat != null){
+                fontList.get(fontList.indexOf(format))
+                        .setActive(format.getFormatId() == activeFormat.getFormatId());
+            }
+        }
+        return fontList;
     }
 
     /**
@@ -249,7 +348,7 @@ public class ContentFormattingHelper {
      * Dispatch update to both content formatting panels and quick actions.
      * @param format updated format to be dispatched
      */
-    public void dispatchUpdate(ContentFormat format){
+    private void dispatchUpdate(ContentFormat format){
         for(StateChangeDispatcher dispatcher: dispatcherList){
             dispatcher.onStateChanged(format);
         }
@@ -275,14 +374,6 @@ public class ContentFormattingHelper {
         return !command.equals(TEXT_FORMAT_TYPE_FONT)
                 && !command.equals(PARAGRAPH_FORMAT_INDENT_DECREASE)
                 && !command.equals(PARAGRAPH_FORMAT_INDENT_INCREASE);
-    }
-
-    /**
-     * Get all content formats
-     * @return list of all content formats
-     */
-    public List<ContentFormat> getAllFormats(){
-        return formatList;
     }
 
 
